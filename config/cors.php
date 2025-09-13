fcb9c22e67c51d181f63f661a061aec19966fc5f
Bola Nabil <bolanabil404@gmail.com>
2025-09-13 12:35:22 +0200

    update frontend path in cors

--- config/cors.php
+++ config/cors.php
@@ -15,12 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['http://localhost:5173/'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
@@ -29,6 +28,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
