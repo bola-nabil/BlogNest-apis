98c4f16ea611fa5ddb588a72bd6f46a66dab47ac
Bola Nabil <bolanabil404@gmail.com>
2025-10-01 13:51:16 +0300

    update userProfile show

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -11,13 +11,24 @@ class UserProfileController extends Controller
     use ApiResponse;
     public function show($id)
     {
-        $user = User::with([
-            'blogs.categories', 
-            'blogs.tags',
-            'comments.blog',
-            'likes.blog'
-        ])->findOrFail($id);
+    $user = User::with([
+        'blogs.categories',
+        'blogs.tags',
+        'comments.blog',
+        'likes.blog'
+    ])->findOrFail($id);
 
-        return $this->success("Successfully fetching user profile", "user", $user);
+    $authUser = auth()->user();
+
+    // check if logged-in user already follows this profile
+    $isFollowing = false;
+    if ($authUser && $authUser->id !== $user->id) {
+        $isFollowing = $authUser->followings()->where('followed_id', $user->id)->exists();
     }
+
+    // add is_following flag
+    $user->is_following = $isFollowing;
+
+    return $this->success("Successfully fetching user profile", "user", $user);
+}
 }
