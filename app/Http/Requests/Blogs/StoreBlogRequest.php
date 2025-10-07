7fe95fb616f297cd49176d35a75631b62392016e
Bola Nabil <bolanabil404@gmail.com>
2025-10-07 19:00:26 +0300

    add tag and category rules

--- app/Http/Requests/Blogs/StoreBlogRequest.php
+++ app/Http/Requests/Blogs/StoreBlogRequest.php
@@ -6,28 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreBlogRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules()
     {
         return [
             "title" => "required|string|max:255",
             "content" => "required|string",
+            "category_id" => "nullable|exists:categories,id",
+            "tags" => "nullable|array",
+            "tags.*" => "exists:tags,id",
             "image" => "nullable|image|mimes:jpg,jpeg,png,gif|max:2048",
-            "slug" => "string"
+            "slug" => "nullable|string",
         ];
     }
 }
