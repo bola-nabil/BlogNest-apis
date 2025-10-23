fc518929cbc3ee36e6bc9e75af8a9113f8ed0048
Bola Nabil <bolanabil404@gmail.com>
2025-10-23 16:19:37 +0300

    update following controller

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -10,66 +10,99 @@ use App\Traits\ApiResponse;
 class FollowController extends Controller
 {
     use ApiResponse;
+
+    /**
+     * Follow a user
+     */
     public function follow($id)
     {
+        $authUser = auth()->user();
         $userToFollow = User::find($id);
-        $user = auth()->user();
 
-        if(!$userToFollow) {
-            return $this->notFound("sorry not found user");
+        if (!$userToFollow) {
+            return $this->notFound("User not found");
         }
 
-        if ($user->id == $userToFollow->id) {
-            return $this->error("You can't follow yourself", 401);
+        if ($authUser->id === $userToFollow->id) {
+            return $this->error("You can't follow yourself", 400);
         }
 
-        if (!$user->followings()->where('following_id', $userToFollow->id)->exists()) {
-            $user->followings()->attach($userToFollow->id);
+        $alreadyFollowing = $authUser->followings()
+            ->where('following_id', $userToFollow->id)
+            ->exists();
 
-            $userToFollow->notify(new UserFollowedNotification($user));
-            return $this->success("Success", "message", "You are now following {$userToFollow->name}");
+        if ($alreadyFollowing) {
+            return $this->success("Success", "message", "You already follow {$userToFollow->name}");
         }
 
-        return $this->success("Success", "message", "You already follow {$userToFollow->name}");
+        // Attach the following
+        $authUser->followings()->attach($userToFollow->id);
+
+        // Notify the followed user
+        $userToFollow->notify(new UserFollowedNotification($authUser));
+
+        return $this->success("Success", "message", "You are now following {$userToFollow->name}");
     }
 
+    /**
+     * Unfollow a user
+     */
     public function unfollow($id)
     {
-        $userToFollow = User::find($id);
-        $user = auth()->user();
+        $authUser = auth()->user();
+        $userToUnfollow = User::find($id);
 
-        if(!$userToFollow) {
-            return $this->notFound("sorry not found user");
+        if (!$userToUnfollow) {
+            return $this->notFound("User not found");
         }
 
-        if ($user->followings()->where('following_id', $userToFollow->id)->exists()) {
-            $user->followings()->detach($userToFollow->id);
+        $isFollowing = $authUser->followings()
+            ->where('following_id', $userToUnfollow->id)
+            ->exists();
 
-            return $this->success("Success", "message", "You unfollowed {$userToFollow->name}");
+        if (!$isFollowing) {
+            return $this->success("Success", "message", "You are not following {$userToUnfollow->name}");
         }
 
-        return $this->success("Success", "message", "You are not following {$userToFollow->name}");
+        // Detach the following
+        $authUser->followings()->detach($userToUnfollow->id);
+
+        return $this->success("Success", "message", "You unfollowed {$userToUnfollow->name}");
     }
 
+    /**
+     * Get followers list for a user
+     */
     public function followers($id)
     {
         $user = User::find($id);
 
-        if(!$user) {
-            return $this->notFound("sorry not found user");
+        if (!$user) {
+            return $this->notFound("User not found");
         }
 
-        return $this->success("successfully fetching followers data", "followers", $user->followers);
+        $followers = $user->followers()
+            ->select('users.id', 'users.name', 'users.username', 'users.profile_image')
+            ->get();
+
+        return $this->success("Followers fetched successfully", "followers", $followers);
     }
 
+    /**
+     * Get followings list for a user
+     */
     public function followings($id)
     {
         $user = User::find($id);
 
-        if(!$user) {
-            return $this->notFound("sorry not found user");
+        if (!$user) {
+            return $this->notFound("User not found");
         }
 
-        return $this->success("successfully fetching followings data", "followings", $user->followings);
+        $followings = $user->followings()
+            ->select('users.id', 'users.name', 'users.username', 'users.profile_image')
+            ->get();
+
+        return $this->success("Followings fetched successfully", "followings", $followings);
     }
 }
