0b594ced8e28352263c6be7f1783aea216b3c6de
Bola Nabil <bolanabil404@gmail.com>
2025-11-01 15:06:55 +0200

    update cors frontend url

--- config/cors.php
+++ config/cors.php
@@ -2,25 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://blognest-94tp.vercel.app'
+        'https://blognest-94tp.vercel.app',
+        'https://blognest-94tp-expxzlca2-bola-nabils-projects.vercel.app',
+        'http://localhost:5173',
     ],
 
     'allowed_origins_patterns' => [],
