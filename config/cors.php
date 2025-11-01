b40fc0a05e639be758aae0b8903544bb6fced792
Bola Nabil <bolanabil404@gmail.com>
2025-11-01 13:35:44 +0200

    update frontend website url

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173'
+        'https://blognest-94tp.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
